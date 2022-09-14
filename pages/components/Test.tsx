509a58c1654b28915c4db0421057d183747cee33
Tim <tim.proctor75@gmail.com>
2022-09-14 23:09:08 +0300

    more wip

--- pages/components/Test.tsx
+++ pages/components/Test.tsx
@@ -2,7 +2,6 @@ import { Box } from "@chakra-ui/react"
 import { createClient, Provider } from 'urql';
 import Query from "./Query";
 import NewCase from "./NewCase";
-
 const client = createClient({
   url: 'https://api.studio.thegraph.com/query/32185/arbit/v0.0.1',
 });
@@ -10,6 +9,7 @@ const client = createClient({
 export default function Test() {
   return (
     <Provider value={client}>
+        {/* <Navbar /> */}
         <Query></Query>
         <Box m={2}>Tomato</Box>
         <NewCase></NewCase>
